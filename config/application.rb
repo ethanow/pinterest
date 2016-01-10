ddb5289d239963d23de3c38dc7a1bd557e0b322f
ethanow <owch0002@e.ntu.edu.sg>
2016-01-11 00:05:36 +0800

    Configured Devise gem

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,9 @@ module Pinterest
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Require for Devise
+    config.assets.initialize_on_precompile = false
+
   end
 end
