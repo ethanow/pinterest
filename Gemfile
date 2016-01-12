0d99de92ae83e9d3c1b4bfdeaa05ce9f2cc01d49
ethanow <owch0002@e.ntu.edu.sg>
2016-01-13 07:51:01 +0800

    Added Paperclip

--- Gemfile
+++ Gemfile
@@ -11,7 +11,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.6'
 gem 'devise', '~> 3.5', '>= 3.5.3'
-
+gem 'paperclip', '~> 4.3', '>= 4.3.2'
 
 gem 'sdoc', '~> 0.4.0', group: :doc
 group :development, :test do
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,4 +81,7 @@ Rails.application.configure do
   #Set to actual hosting
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  # Paperclip config:
+Paperclip.options[:image_magick_path] = "/opt/ImageMagick/bin"
+Paperclip.options[:command_path] = "/opt/ImageMagick/bin"
 end
